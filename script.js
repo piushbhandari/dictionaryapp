b1904019c3ab7e25ab7e433541f3cb3e5e4a69aa
piush.bhandari <piush.bhandari@americaneagle.com>
2024-01-08 12:39:01 -0600

    adding basic form validation to the search

--- script.js
+++ script.js
@@ -1,94 +1,129 @@
 // set up togglers (i.e choosing fonts)
 (function () {
-  const togglers = [...document.querySelectorAll("[data-expander-toggle]")];
+	const togglers = [...document.querySelectorAll("[data-expander-toggle]")];
 
-  togglers.forEach((toggler) => {
-    const toggleAttributeValue = toggler.getAttribute("data-expander-toggle");
-    const toggleTarget = document.getElementById(toggleAttributeValue);
+	togglers.forEach((toggler) => {
+		const toggleAttributeValue = toggler.getAttribute(
+			"data-expander-toggle"
+		);
+		const toggleTarget = document.getElementById(toggleAttributeValue);
 
-    toggler.addEventListener("click", (e) => {
-      if (!toggler.classList.contains("active")) {
-        toggler.classList.add("active");
-        toggler.setAttribute("aria-expanded", "true");
-        toggleTarget.classList.add("active");
-      } else if (toggler.classList.contains("active")) {
-        toggler.classList.remove("active");
-        toggler.setAttribute("aria-expanded", "false");
-        toggleTarget.classList.remove("active");
-      }
-    });
-  });
+		toggler.addEventListener("click", (e) => {
+			if (!toggler.classList.contains("active")) {
+				toggler.classList.add("active");
+				toggler.setAttribute("aria-expanded", "true");
+				toggleTarget.classList.add("active");
+			} else if (toggler.classList.contains("active")) {
+				toggler.classList.remove("active");
+				toggler.setAttribute("aria-expanded", "false");
+				toggleTarget.classList.remove("active");
+			}
+		});
+	});
 })();
 
 // selecting page font
 (function () {
-  const mainFontBtn = document.querySelector(
-    "[data-expander-toggle='font-flyout']"
-  );
-  const mainFontBtnText = document.querySelector("#font-button-text");
-  const fontSelectorButtons = [
-    ...document.querySelectorAll("[ font-selector]"),
-  ];
+	const mainFontBtn = document.querySelector(
+		"[data-expander-toggle='font-flyout']"
+	);
+	const mainFontBtnText = document.querySelector("#font-button-text");
+	const fontSelectorButtons = [
+		...document.querySelectorAll("[ font-selector]"),
+	];
 
-  let bodyclass = "";
-  loadPageFont();
+	let bodyclass = "";
+	loadPageFont();
 
-  fontSelectorButtons.forEach((btn) => {
-    const font = btn.value;
+	fontSelectorButtons.forEach((btn) => {
+		const font = btn.value;
 
-    btn.addEventListener("click", (e) => {
-      setupFonts(font);
-      localStorage.setItem("pageFont", font);
-    });
-  });
+		btn.addEventListener("click", (e) => {
+			setupFonts(font);
+			localStorage.setItem("pageFont", font);
+		});
+	});
 
-  function loadPageFont() {
-    const storedFont = localStorage.getItem("pageFont");
-    const firstFont = document.querySelector("[font-selector]").value;
-    storedFont === null ? setupFonts(firstFont) : setupFonts(storedFont);
-  }
-  function setupFonts(font) {
-    mainFontBtn.value = font;
-    mainFontBtnText.textContent = font;
+	function loadPageFont() {
+		const storedFont = localStorage.getItem("pageFont");
+		const firstFont = document.querySelector("[font-selector]").value;
+		storedFont === null ? setupFonts(firstFont) : setupFonts(storedFont);
+	}
+	function setupFonts(font) {
+		mainFontBtn.value = font;
+		mainFontBtnText.textContent = font;
 
-    if (font === "Sans Serif") {
-      bodyclass = "sans";
-    } else if (font === "Serif") {
-      bodyclass = "serif";
-    } else if (font === "Mono") {
-      bodyclass = "mono";
-    }
+		if (font === "Sans Serif") {
+			bodyclass = "sans";
+		} else if (font === "Serif") {
+			bodyclass = "serif";
+		} else if (font === "Mono") {
+			bodyclass = "mono";
+		}
 
-    document.body.setAttribute("class", bodyclass);
-  }
+		document.body.setAttribute("class", bodyclass);
+	}
 })();
 
 // theme switcher
 
 (function () {
-  const themeSwitcher = document.querySelector("[data-theme]");
+	const themeSwitcher = document.querySelector("[data-theme]");
 
-  const storedTheme = localStorage.getItem("pageTheme");
+	const storedTheme = localStorage.getItem("pageTheme");
 
-  if (storedTheme !== null) {
-    setupThemes(storedTheme);
-  }
+	if (storedTheme !== null) {
+		setupThemes(storedTheme);
+	}
 
-  themeSwitcher.addEventListener("click", (e) => {
-    const currentTheme = themeSwitcher.getAttribute("data-theme");
-    setupThemes(currentTheme);
-  });
+	themeSwitcher.addEventListener("click", (e) => {
+		const currentTheme = themeSwitcher.getAttribute("data-theme");
+		setupThemes(currentTheme);
+	});
 
-  function setupThemes(theme) {
-    if (theme === "light") {
-      document.body.setAttribute("id", "dark");
-      themeSwitcher.setAttribute("data-theme", "dark");
-    } else if (theme === "dark") {
-      document.body.setAttribute("id", "light");
-      themeSwitcher.setAttribute("data-theme", "light");
-    }
+	function setupThemes(theme) {
+		if (theme === "light") {
+			document.body.setAttribute("id", "dark");
+			themeSwitcher.setAttribute("data-theme", "dark");
+		} else if (theme === "dark") {
+			document.body.setAttribute("id", "light");
+			themeSwitcher.setAttribute("data-theme", "light");
+		}
 
-    localStorage.setItem("pageTheme", theme);
-  }
+		localStorage.setItem("pageTheme", theme);
+	}
 })();
-// utility functions
+
+// dictionary api: https://api.dictionaryapi.dev/api/v2/entries/en/<word>
+// source: https://dictionaryapi.dev/
+
+(function () {
+	const searchInput = document.querySelector("#dictionary-search");
+	const searchButton = document.querySelector("#search-btn");
+	const errMsg = document.querySelector("#error-msg");
+
+	let currentInputValue = "";
+	searchButton.addEventListener("click", (e) => {
+		currentInputValue = searchInput.value;
+		validateForm();
+	});
+
+	function validateForm() {
+		if (!currentInputValue.trim().length) {
+			errMsg.textContent = "Whoops, can’t be empty…";
+			setupStyles(true);
+		} else {
+			setupStyles(false);
+		}
+	}
+
+	function setupStyles(flag) {
+		if (flag) {
+			errMsg.classList.add("error");
+			searchInput.classList.add("error");
+		} else {
+			errMsg.classList.remove("error");
+			searchInput.classList.remove("error");
+		}
+	}
+})();
